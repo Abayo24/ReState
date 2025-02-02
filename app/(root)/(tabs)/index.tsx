c39db0235e0307dca7875b790666787805be62dd
Abayo24 <abayo.akinyi@riarauniversity.ac.ke>
2025-02-02 19:11:07 +0300

    first commit

---
+++ app/(root)/(tabs)/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Link } from 'expo-router';
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text className="font-bold my-10 font-rubik text-3xl">Welcome to ReState</Text>
+      <Link href="/sign-in">Sign in</Link>
+      <Link href="/explore">Explore</Link>
+      <Link href="/profile">Profile</Link>
+      <Link href="/properties/1">Property</Link>
+    </View>
+  );
+}
